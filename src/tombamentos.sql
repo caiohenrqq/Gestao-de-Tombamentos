22eae1a78ed9b81465e5ca8efeabe0b2536f0883
henrqq <caiohenrqq@gmail.com>
2024-09-18 12:55:51 -0400

    adiciona database tombamentos_id em tombamentos.sql

--- src/tombamentos.sql
+++ src/tombamentos.sql
@@ -1,14 +1,25 @@
 CREATE DATABASE tombamentos;
-USE tombamentos
+USE tombamentos;
 
-CREATE TABLE tecnicos
-(
-	id int unsigned not null auto_increment,
-    nome varchar(50) not null,
-    email varchar(50) not null,
-    PRIMARY KEY(id)
+CREATE TABLE tecnicos (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 ALTER TABLE tecnicos 
 ADD COLUMN senha varchar(255) not null;
-	
+    
+SELECT * FROM tecnicos;
+
+CREATE TABLE tombamentos (
+  tombamento_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  secretaria VARCHAR(50) NOT NULL,
+  tecnico VARCHAR(50) NOT NULL,
+  entrada DATETIME NOT NULL,
+  saida DATETIME NOT NULL,
+  prioridade INT UNSIGNED NOT NULL,
+  status INT UNSIGNED NOT NULL,
+  PRIMARY KEY (tombamento_id)
+);
